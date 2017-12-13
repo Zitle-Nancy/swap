2d33456053e4447508f4ecaf84695bc0a96018e6
Zitle-Nancy <ancizj@hotmail.com>
2017-12-12 18:53:59 -0600

    agrega la estructura de las carpetas y obtiene los datos del API cURL

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function() {
+	var loadPage = function () {
+		getData();
+	};
+	var getData = function() {
+		$.ajax({
+			type: "POST",
+			url: 'https://parseapi.back4app.com/functions/usuario',
+			dataType : "json",
+			headers: {
+				'cache-control': 'no-cache',
+				'content-type': 'application/json',
+				'x-parse-application-id': 'KthkFDEcuDZ1on1BufkemQmZ1Ze2OXDucria1E2s',
+				'x-parse-rest-api-key': 'KoChaj2LH7bvkKfm50Rbr8QLRy9FtmKkTopSrnHd'				
+			},
+			data:JSON.stringify({
+				userId :'57luWV0QL4'
+			}),
+			success: function( result ) {
+				console.log(result)
+			}, 
+			error: function( error ) {
+				console.error('error', arguments);
+			}
+		});
+	}
+
+	$(document).ready(loadPage);
+})();
