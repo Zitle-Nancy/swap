86913085b8384df12e6d5d2e9434efb7ed75a887
Zitle-Nancy <ancizj@hotmail.com>
2017-12-14 16:12:44 -0600

    obtiene el nombre y la foto de perfil del usuario

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 (function() {
+	
+	var urlPathName = $(location).attr('pathname');
+	var idUser = urlPathName.replace('/','');
+	var imgProfile = $('#img-profile');
+	var userName = $('#user-name');
 	var loadPage = function () {
+        Materialize.updateTextFields();
 		getUserData();
 	};
+
 	var getUserData = function() {
 		$.ajax({
 			type: "POST",
@@ -13,17 +20,21 @@
 				'x-parse-application-id': 'KthkFDEcuDZ1on1BufkemQmZ1Ze2OXDucria1E2s',
 				'x-parse-rest-api-key': 'KoChaj2LH7bvkKfm50Rbr8QLRy9FtmKkTopSrnHd'				
 			},
-			data:JSON.stringify({
-				userId :'57luWV0QL4'
+			data: JSON.stringify({
+				"userId" : idUser
+
 			}),
 			success: function( result ) {
-				console.log(result)
+				console.log(result);
+				var name = result.result.firstName + " " + result.result.lastName;
+				imgProfile.attr('src',result.result.image);
+				userName.text(name);
 			}, 
 			error: function( error ) {
 				console.error('error', arguments);
 			}
 		});
-	}
+	}; 
 
 	$(document).ready(loadPage);
 })();
